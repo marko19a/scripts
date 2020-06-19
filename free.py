6c5ed1b01dc99852393c8cf67b2710efa5c6bfcd
marko19a <marko19a97@gmail.com>
2020-06-19 18:42:01 +0200

    changed free to show cpu mhz

--- free.py
+++ free.py
@@ -7,4 +7,22 @@ lines = file.readlines()
 total = float(re.search(r'\d+', lines[0]).group()) / (1024 * 1024)
 av = float(re.search(r'\d+', lines[2]).group()) / (1024 * 1024)
 
-subprocess.call(['notify-send', 'Memory', 'Available: %.2f (%d%s)' % (av, int(av/total*100), '%')])
+file.close()
+file = open('/proc/cpuinfo')
+lines = file.readlines()
+
+mhz = float(re.search(r'\d+', lines[7]).group())
+'''
+file.close()
+file = open('/proc/stat')
+line = file.readline()
+
+values = re.findall(r'\d+', line)
+
+values = [int(v) for v in values]
+
+p = (values[3] * 20) / (sum(values[:3]) + sum(values[4:]))
+'''
+file.close()
+subprocess.call(
+    ['notify-send', 'Memory', 'MEM: %.2f (%d%s) | CPU: %d MHz' % (av, int(av / total * 100), '%', mhz)])
