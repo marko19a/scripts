7f242c8b88e2c29743c81d803ef384280baa6ae3
mmilosevic2 <marko.milosevic@euroicc.com>
2021-12-10 10:26:36 +0100

    fix

--- play.py
+++ play.py
@@ -1,42 +1,58 @@
+import os
+import re
 import sys
 import subprocess
-import re
+import pathlib
+
+player = 'mplayer'
+play_file = f'{pathlib.Path.home()}/.play'
+filetypes = (
+    '265 26l 3ga 3gp 3p2 4xm aac asf avi buk cpk dat ' +
+    'h264 ifv kmv m2t mj2 mjp mjp2 mk3d mka mkv mov mp3 mp4 ' +
+    'mpeg mpg4 mqv nut ogg ogm qt rm roq smi ssa ts vc1 vivo ' +
+    'vob vp3 vro webm wma wmv'
+).split(' ')
+
+def isVid(file):
+    for type in filetypes:
+        if type in file:
+            return True
+
+    return False
 
 if len(sys.argv) == 1:
-    file = open('/home/marko/.play', 'r')
-    line = file.readline()
-    x = re.search('E\d\d', line)
+    with open(play_file, 'r') as f:
+        line = f.readline()
+
+    x = re.search('E|e\d\d', line)
     x = x.span()[0] + 1
-    ep = line[x:x+2]
-    iep = int(ep) + 1
+    ep = int(line[x:x+2]) + 1
 
-    line = list(line)
-    file.close()
-    file = open('/home/marko/.play', 'w')
-    if iep <= 9:
-        line[x] = '0'
-        line[x+1] = f'{iep}'
-        line = ''.join(line)
-    else:
-        line[x] = f'{iep}'
-        line[x + 1] = ''
-        line = ''.join(line)
+    parent = pathlib.Path(line).parent
+    eps = os.listdir(parent)
+    eps = list(filter(isVid, eps))
+    eps.sort(key=lambda x: x.upper())
+    
+    if ep-1 >= len(eps):
+        print(f'No more eps last is {ep-1:02d}')
+        exit()
 
-    file.write(line)
-    file.close()
+    next = f'{parent}/{eps[ep-1]}'
+    with open(play_file, 'w') as f:
+        f.write(next)
 
-    subprocess.call(['mplayer', line])
+    subprocess.call([player, next])
 
 else:
     if sys.argv[1] == 'r':
-        file = open('/home/marko/.play')
-        line = file.readline()
-        file.close()
+        with open(play_file, 'r') as f:
+            line = f.readline()
 
-        subprocess.call(['mplayer', line])
+        subprocess.call([player, line])
     else:
-        file = open('/home/marko/.play', 'w')
-        file.write(sys.argv[1])
-        file.close()
+        with open(play_file, 'w') as f:
+            fpath = pathlib.Path(sys.argv[1]).absolute().__str__()
+            print(fpath)
+            f.write(fpath)
 
-        subprocess.call(['mplayer', sys.argv[1]])
+        subprocess.call([player, sys.argv[1]])
